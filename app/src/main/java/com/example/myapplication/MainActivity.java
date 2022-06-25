cd88d5d4a8a782b64393a70a3f4c3aabd6053f38
Atmaja19 <maulidakrisna0601@gmail.com>
2022-06-25 21:32:47 +0700

    MODUL 2 PPB

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.brother,R.drawable.event,R.drawable.family, R.drawable.malang};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.images);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    String data1,data2;
+    int myImage;
+
+    ImageView mainImageView;
+    TextView title, description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        mainImageView = findViewById(R.id.mainImageView);
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+
+        getData();
+        setData();
+    }
+    private void getData(){
+        if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")){
+            data1 = getIntent().getStringExtra("data1");
+            data2 = getIntent().getStringExtra("data2");
+            myImage = getIntent().getIntExtra("myImage",1);
+        }else {
+            Toast.makeText(this,"Tidak Ada Data", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setData(){
+        title.setText(data1);
+        description.setText(data2);
+        mainImageView.setImageResource(myImage);
+    }
+}
